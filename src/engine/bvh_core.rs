d7823f21ea65a9a6582c0a6397baac612edc8b68
btasc <176724843+btasc@users.noreply.github.com>
2025-12-26 18:43:51 -0500

    Refactored sort_sweep_vertices

--- src/engine/bvh_core.rs
+++ src/engine/bvh_core.rs
@@ -8,6 +8,8 @@ use crate::gpu::buffers::{
     GpuStorageVertex as Vertex,
 };
 
+const DEFAULT_TRI_RGBA: [f32; 4] = [0.5, 0.5, 1.0, 1.0];
+
 // I make this into a type so that if I ever decide to change it to like Vec<f32> or like Vec<[[f32; 3]; 3]> I can just change the type
 // Its raw since we don't use our already made Vertex struct, its being taken directly from a file
 pub type RawTriangleList = Vec<[f32; 9]>;
@@ -23,13 +25,17 @@ pub struct BvhRes {
 // This is our public entrypoint to building a BVH
 // This takes all the work from starting at raw vertex list to a full write set for the buffers
 pub fn build_mesh_bvh(raw_triangles: RawTriangleList) -> BvhRes {
-    // First, we create a hashmap with the vertices to reduce memory
+    // First, we index the vertices and the triangles for memory saving
     let (vertices, triangles) = sort_sweep_vertices(raw_triangles);
+
+    // todo past this
 
     BvhRes {
         vertices,
         triangles,
 
+        blas_tree: vec![],
+        blas_leaves: vec![],
     }
 }
 
@@ -45,82 +51,110 @@ struct VertexTriPos {
 }
 
 fn sort_sweep_vertices(raw_triangles: RawTriangleList) -> (Vec<Vertex>, Vec<TriangleData>) {
+    let tri_count = raw_triangles.len();
+
     // We sort it with rayon
     // In this case we use PDQSort, a rust provided method, sped up with rayon multithreading
     // We could use radix sort, but that would only be faster for massive meshes, and it is difficult to use with floats
     let mut indexed_vertices: Vec<Vertex> = Vec::new();
-    let mut indexed_triangles: Vec<TriangleData> = Vec::new();
 
-    let mut vertex_tri_pos_vec: Vec<VertexTriPos> = Vec::new();
-    vertex_tri_pos_vec.reserve(raw_triangles.len() * 3);
+    let mut indexed_triangles: Vec<TriangleData> = (0..tri_count)
+        .map(|_| TriangleData {
+            vertices: [0, 0, 0],
+            _pad: 0,
+            rgba: DEFAULT_TRI_RGBA,
+        })
+        .collect();
 
-    for raw_triangle in raw_triangles.into_iter().enumerate() {
-        for triple_tri_idx in 0..3 {
-            // Goes through every vertex in the triangle
-            let vertex: [f32; 3] = [
-                raw_triangle.1[triple_tri_idx * 3],
-                raw_triangle.1[triple_tri_idx * 3 + 1],
-                raw_triangle.1[triple_tri_idx * 3 + 2],
-            ];
+    // This is used to keep track of how many vertices have been linked back to this triangle
+    // At the end, it should be entirely a list of 3's
+    let mut indexed_triangles_used_tally: Vec<u8> = vec![0; tri_count];
 
-            let original_tri_idx = raw_triangle.0;
+    let mut vertex_tri_pos_vec: Vec<VertexTriPos> = Vec::with_capacity(tri_count * 3);
 
-            let vertex_tri_pos = VertexTriPos { coordinate: vertex, tri_idx: original_tri_idx };
-
-            vertex_tri_pos_vec.push(vertex_tri_pos);
+    // Here we build the list of VertexTriPos-s to sort later
+    for (original_tri_idx, raw_triangle) in raw_triangles.iter().enumerate() {
+        for i in 0..3 {
+            vertex_tri_pos_vec.push(VertexTriPos {
+                coordinate: [
+                    raw_triangle[i * 3],
+                    raw_triangle[i * 3 + 1],
+                    raw_triangle[i * 3 + 2],
+                ],
+                tri_idx: original_tri_idx,
+            });
         }
     }
 
-    // We use our custom compare floats to compare the triples
-    vertex_tri_pos_vec.par_sort_unstable_by(|a, b| compare_floats(a.coordinate, b.coordinate));
+    // We use rayon par sort to multithread to speed up the process
+    // We also use .total_cmp because it handles NANs and -0s safely, incase any issues are with the raw triangles
+    // We use sort by because, with the struct, we have to have custom logic
+    vertex_tri_pos_vec.par_sort_unstable_by(|a, b| {
+        a.coordinate[0].total_cmp(&b.coordinate[0])
+            .then_with(|| a.coordinate[1].total_cmp(&b.coordinate[1]))
+            .then_with(|| a.coordinate[2].total_cmp(&b.coordinate[2]))
+    });
 
     // Now we go through and fill out indexed_vertices
     // Tally of num of groups
     // This gives us our index to the current group of vertex
-    let mut group_tally: usize = 0;
+    let mut current_vertex_group_idx: u32 = 0;
 
-    for idx in 0..vertex_tri_pos_vec.len() {
+    // We run our first pass for the first element
+    // We make sure it's not empty before we do this. There are a lot of other ways to check, but it doesn't really matter
+    if !vertex_tri_pos_vec.is_empty() {
+        // We recreate the first variable to make the code easier to read
+        let vertex_idx: usize = 0;
 
-        // We have 0 map to NAN so that it will always be equal to the current coordinate
-        // See compare floats for this logic
+        let tri_idx = vertex_tri_pos_vec[vertex_idx].tri_idx;
+        let current_pos = vertex_tri_pos_vec[vertex_idx].coordinate;
 
-        let previous_vertex_pos: [f32; 3] = match idx {
-            0 => [f32::NAN, f32::NAN, f32::NAN],
-            _ => vertex_tri_pos_vec[idx - 1].coordinate,
-        };
+        // We push it since its the first
+        indexed_vertices.push(Vertex::from_arr(current_pos));
 
-        let current_vertex_pos: [f32; 3] = vertex_tri_pos_vec[idx].coordinate;
+        // We only have one path since its the first, so we skip all the equal logic
+        let triangle_vertex_count = indexed_triangles_used_tally[tri_idx];
+        indexed_triangles[tri_idx].vertices[triangle_vertex_count as usize] = current_vertex_group_idx;
 
-        match compare_floats(previous_vertex_pos, current_vertex_pos) {
-            // Equal
-            // We are in the same group, so we reuse our group_tally
-            std::cmp::Ordering::Equal => (),
-            // Not equal (either greater or less)
-            // This means we are in a new group
-            _ => {
-                group_tally += 1;
-                indexed_vertices.push(Vertex::from_arr(current_vertex_pos));
-            },
+        // We inc the tally since we used it
+        indexed_triangles_used_tally[tri_idx] += 1;
+    }
+
+    // We start at one as we already handled the first element
+    for vertex_idx in 1..vertex_tri_pos_vec.len() {
+        let tri_idx = vertex_tri_pos_vec[vertex_idx].tri_idx;
+
+        // We can safely index into previous pos as we start at 1
+        let current_pos = vertex_tri_pos_vec[vertex_idx].coordinate;
+        let previous_pos = vertex_tri_pos_vec[vertex_idx - 1].coordinate;
+
+        let is_equal_pos: bool =
+            current_pos[0] == previous_pos[0] &&
+            current_pos[1] == previous_pos[1] &&
+            current_pos[2] == previous_pos[2];
+
+        // If we are on a new group of vertx, we inc the group idx and push that new vertex
+        if !is_equal_pos {
+            current_vertex_group_idx += 1;
+            indexed_vertices.push(Vertex::from_arr(current_pos));
         }
 
+        let triangle_vertex_count = indexed_triangles_used_tally[tri_idx];
+        indexed_triangles[tri_idx].vertices[triangle_vertex_count as usize] = current_vertex_group_idx;
 
+        // We inc the tally since we used it
+        indexed_triangles_used_tally[tri_idx] += 1;
+    }
+
+    // This assertion checks that all the tallies are at 3
+    // Since the tallies track the num of vertices attached to each triangle, something has gone wrong if a triangle doesnt have 3 vertices
+    // We also keep this in debug just for better release performance, as this is the sort of thing that shouldn't fail assuming its done well
+    #[cfg(debug_assertions)]
+    {
+        for tri_tally in indexed_triangles_used_tally {
+            assert_eq!(tri_tally, 3, "Tri used tally is not fully set to 3 at end of loop. Assertion failed");
+        }
     }
 
     (indexed_vertices, indexed_triangles)
-}
-
-// Compare 3 floats lexigraphically
-// Go float by float of the 3, returning equal if they all are the same
-fn compare_floats(a: [f32; 3], b: [f32; 3]) -> std::cmp::Ordering {
-    for i in 0..3 {
-        // partial_cmp returns None if float is NaN.
-        let ord = a[i].partial_cmp(&b[i]).unwrap_or(std::cmp::Ordering::Equal);
-
-        // if they aren't equal, we return that
-        if ord != std::cmp::Ordering::Equal {
-            return ord;
-        }
-    }
-    // If X, Y, and Z are all equal
-    std::cmp::Ordering::Equal
 }
